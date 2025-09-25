7826cec5b5e1ec589682f07a14aa4b4240f4f6d8
B4skervill3 <picardal.axylljudd@gmail.com>
2025-09-25 19:03:31 +0800

    stopped following along around f"

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0tr949$4ixq(v0ghkd^_g9#op+6$y!511r-@o@bw#6mr1bz!i0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
